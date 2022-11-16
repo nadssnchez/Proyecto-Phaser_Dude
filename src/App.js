45cb1f43c20fe4dead7b29d449693828959f4a53
Nadia Rosario Sanchez <nadiarossario@gmail.com>
2022-11-16 20:02:03 -0300

    Juego dude. Nadia Rosario Sanchez

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import Phaser from 'phaser';
+import { useState, useEffect } from 'react';
+import Escena from './components/Escena';
+
+function App() {
+
+  const [listo, setListo] = useState(false); // uso state de una variable Listo, se no se usa esto los lienzos se acumularan en la vista
+
+  useEffect(() => { // uso el hook para renderizar acciones que el react no hace, se ejecuta mediante un estado
+    const config = {
+      type: Phaser.AUTO,
+      width: 800, // ancho del lienzo
+      height: 600, // alto del lienzo
+      physics: {
+        default: 'arcade',
+        arcade: {
+          gravity: { y: 300 }, // cambia la gravedad en el eje Y
+          debug: false
+        }
+      },
+      scene: [Escena]
+    };
+    // arranca el juego
+    const game = new Phaser.Game(config); // a la variable game se le asigna un objeto del tipo phaser
+
+    game.events.on("LISTO", setListo); // trigger cuando el juego esta completamente listo
+
+    // si no pongo esto, se acumulan duplicados del lienzo
+    return () => {
+      setListo(false);
+      game.destroy(true);
+    }
+  }, [listo]);
+} 
+
+export default App;
